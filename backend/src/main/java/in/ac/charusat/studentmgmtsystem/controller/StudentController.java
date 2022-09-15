3e9c65efc7b29ff8cb13d40dfa162d0759cf75f1
yash-shah17 <88420125+yash-shah17@users.noreply.github.com>
2022-09-16 01:30:20 +0530

    Student entities

--- backend/src/main/java/in/ac/charusat/studentmgmtsystem/controller/StudentController.java
+++ backend/src/main/java/in/ac/charusat/studentmgmtsystem/controller/StudentController.java
@@ -1,10 +1,14 @@
 package in.ac.charusat.studentmgmtsystem.controller;
 
+import in.ac.charusat.studentmgmtsystem.model.Student;
+import in.ac.charusat.studentmgmtsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StudentController {
@@ -16,12 +20,25 @@ public class StudentController {
             )
     );
 
+    @Autowired
+    StudentRepository studentRepository;
+//    List<Student> students = new ArrayList<>(
+//            Arrays.asList(
+//                    new Student(1, "Tom", "US"),
+//                    new Student(2, "Harry", "Canada"),
+//                    new Student(3, "Nick", "UK")
+//            )
+//    );
+
     // Mappings - URL endpoints
     // Get the list of all student
     @GetMapping("/listStudents")
     public List<Student> getAllStudents(){
         return students;
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
     }
+
     // Get the student information
     @GetMapping("/student/{id}")
     public Student getStudent(@PathVariable Integer id){
@@ -31,7 +48,10 @@ public class StudentController {
             }
         }
         return null;
+    public Student getStudent(@PathVariable Integer id) {
+        return studentRepository.findById(id).get();
     }
+
     // Delete the student
     @DeleteMapping("/student/{id}")
     public List<Student> deleteStudent(@PathVariable Integer id){
@@ -41,13 +61,21 @@ public class StudentController {
             }
         }
         return students;
+    public List<Student> deleteStudent(@PathVariable Integer id) {
+        studentRepository.delete(studentRepository.findById(id).get());
+        return studentRepository.findAll();
     }
+
     // Add new student
     @PostMapping("/student")
     public List<Student> addStudent(@RequestBody Student student){
         students.add(student);
         return students;
+    public List<Student> addStudent(@RequestBody Student student) {
+        studentRepository.save(student);
+        return studentRepository.findAll();
     }
+
     // Update the student information
     @PutMapping("/student/{id}")
     public List<Student> updateStudent(@RequestBody Student student, @PathVariable Integer id){
@@ -58,6 +86,12 @@ public class StudentController {
             }
         }
         return students;
+    public List<Student> updateStudent(@RequestBody Student student, @PathVariable Integer id) {
+        Student studentObj = studentRepository.findById(id).get();
+        studentObj.setName(student.getName());
+        studentObj.setAddress(student.getAddress());
+        studentRepository.save(studentObj);
+        return studentRepository.findAll();
     }
 
 }
